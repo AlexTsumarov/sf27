45c91757265a95a8c64ab58ffb67a4373ad1277d
Alexandr <Alexandr_Tsumarov1@epam.com>
2015-11-12 11:00:56 +0300

    SF27 Demo

---
+++ src/AppBundle/Entity/t1.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Description of t1
+ *
+ * @author Alexandr_Tsumarov1
+ */
+class t1 implements UserInterface{
+	/**
+	 * 
+	 */
+	public function eraseCredentials(Array $aParams) {
+		
+		return $aParams;
+	}
+
+	public function getPassword() {
+		
+	}
+
+	public function getRoles() {
+		
+	}
+
+	public function getSalt() {
+		
+	}
+
+	public function getUsername() {
+		
+	}
+
+//put your code here
+}
